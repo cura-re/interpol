c83790a347968e6ee33633bd23724f20a918e94a
Blehjo <blehjooo@gmail.com>
2023-11-25 20:51:23 -0600

    fixed audio querie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import { Component } from "react";
-
-
+import WaveSurfer from 'wavesurfer.js'
+import Regions from 'wavesurfer.js/dist/plugins/regions.js'
 
 class Index extends Component {
+
   render() {
+    const wavesurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: '#4F4A85',
+      progressColor: '#383351',
+      url: '/audio.mp3',
+    });
     return (
       <h1>Hello</h1>
-    )
+      w
+    );
   }
 }
 

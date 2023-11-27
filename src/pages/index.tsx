1a80849fd1859765ed8766f088b8578c7c080311
bseton11-YJKnqQ <bseton11@gmail.com>
2023-11-26 18:15:19 -0600

    configured wavesurfer js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,53 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 import WaveSurfer from 'wavesurfer.js'
-import Regions from 'wavesurfer.js/dist/plugins/regions.js'
+import { PlayButton } from "../styles/waveform/waveform.styles";
 
-class Index extends Component {
+interface IState {
+  playing: boolean;
+
+}
+class Index extends Component<{}, IState> {
+  private wavesurfer: WaveSurfer | null = null;
+  private waveformRef = createRef<HTMLDivElement>();
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      playing: false
+    }
+  }
+
+  handlePlay = () => {
+    this.setState({ playing: !this.state.playing });
+    this.wavesurfer?.playPause();
+  }
+
+  componentDidMount() {
+    // Initialize WaveSurfer
+    this.wavesurfer = WaveSurfer.create({
+      container: this.waveformRef.current!,
+      waveColor: "white",
+      progressColor: 'purple',
+    });
+
+    // Load audio file
+    this.wavesurfer.load("https://api.twilio.com//2010-04-01/Accounts/AC25aa00521bfac6d667f13fec086072df/Recordings/RE6d44bc34911342ce03d6ad290b66580c.mp3");
+  }
+
+  componentWillUnmount() {
+    // Clean up when the component is unmounted
+    if (this.wavesurfer) {
+      this.wavesurfer.destroy();
+    }
+  }
 
   render() {
-    const wavesurfer = WaveSurfer.create({
-      container: '#waveform',
-      waveColor: '#4F4A85',
-      progressColor: '#383351',
-      url: '/audio.mp3',
-    });
     return (
-      <h1>Hello</h1>
-      w
+      <div>
+        <PlayButton onClick={this.handlePlay}>
+          {!this.state.playing ? "Play" : "Pause"}
+        </PlayButton>
+        <div ref={this.waveformRef}></div>
+      </div>
     );
   }
 }
